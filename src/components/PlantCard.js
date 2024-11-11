9767c5148119c61df368ff469f8b09e4cb2e8e3a
Basil Wabuke <babazwabuke4@gmail.com>
2024-11-11 12:41:21 +0300

    initial commit

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, updatePrice, deletePlant }) {
+  const [isSoldOut, setIsSoldOut] = useState(false);
+  const [newPrice, setNewPrice] = useState(plant.price);
+
+  const toggleSoldOut = () => {
+    setIsSoldOut(!isSoldOut);
+  };
+
+  const handlePriceChange = (e) => {
+    setNewPrice(e.target.value);
+  };
+
+  const handleUpdatePrice = () => {
+    updatePrice(plant.id, parseFloat(newPrice));
+  };
+
+  const handleDelete = () => {
+    deletePlant(plant.id);
+  };
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price}</p>
+      <input
+        type="number"
+        value={newPrice}
+        onChange={handlePriceChange}
+        step="0.01"
+        min="0"
+      />
+      <button onClick={handleUpdatePrice}>Update Price</button>
+      <button className={isSoldOut ? "" : "primary"} onClick={toggleSoldOut}>
+        {isSoldOut ? "Sold Out" : "In Stock"}
+      </button>
+      <button onClick={handleDelete} className="delete">
+        Delete
+      </button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -3,12 +3,14 @@ import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, isLoading, addPlant, searchPlants, updatePrice, deletePlant }) {
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant} />
+      <Search searchPlants={searchPlants} />
+      <PlantList plants={plants} updatePrice={updatePrice} deletePlant={deletePlant} />
     </main>
   );
 }
